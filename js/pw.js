40818d51dc0995a67f8d5a5b1fefaf31b69a4a59
EmilyHomNici <ehomnici@gmail.com>
2017-08-15 23:25:55 -0500

    + pw protection on one project

---
+++ js/pw.js
@@ -0,0 +1,9 @@
+
+    var password;
+    var pass1="cool";
+    password=prompt('Please enter your password to view this page!',' ');
+    if (password===pass1) {
+      alert('Password Correct! Click OK to enter!');
+    } else {
+      window.location="index.html";
+    }
