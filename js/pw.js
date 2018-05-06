074f92c3aed940c64a4d3e09ab19db44f4dae02b
EmilyHomNici <ehomnici@gmail.com>
2018-05-06 10:47:44 -0500

    update to bootstrap 4. Update to simple nav. Update accordians.

--- js/pw.js
+++
@@ -1,9 +0,0 @@
-
-    var password;
-    var pass1="cool";
-    password=prompt('Please enter your password to view this page!',' ');
-    if (password===test) {
-      alert('Password Correct! Click OK to enter!');
-    } else {
-      window.location="index.html";
-    }
